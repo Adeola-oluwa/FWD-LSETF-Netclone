06ee9ab0ac45a78e623757d4366877d4ec3c7db2
Bello Israel <belloisrael1999@gmail.com>
2024-06-08 02:47:54 -0700

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
 
 const Navbar = () => {
   return (
+    <>
     <div className='navbar'>
+      <div className='navbar-left'>
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} width={20} alt="" className='icons' />
+        <p>Childern</p>
+
+        <img src={bell_icon} width={20} alt="" className='icons' />
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" className='profile' />
+          <img src={caret_icon} width={10} alt="" />
+        </div>
+      </div>
+      
       
     </div>
+
+    </>
   )
 }
 
