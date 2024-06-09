a61958144c004ee652f3882f791f88d7c71a675b
Bello Israel <belloisrael1999@gmail.com>
2024-06-09 15:11:15 -0700

    tiile card almost done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
+
 
 
 const Home = () => {
@@ -13,7 +17,18 @@ const Home = () => {
       <div className="hero-caption">
         <img src={hero_title} alt="" className='caption-img' />
         <p>Discovering his ties to secret ancient irder, a young man living in modern Istanbul embarks on quest to save the city from an importal enemy.</p>
+        <div className="hero-btns">
+          <button className='btn'><img src={play_icon} alt="" />Play</button>
+          <button className='btn dark-btn'><img src={info_icon} alt="" /> More Info</button>
+        </div>
+        <TitleCards/> 
       </div>
+    </div>
+    <div className="more-cards">
+    <TitleCards title= {"Blockbuster Movies"}/> 
+    <TitleCards title= {"Only on Neflix"}/> 
+    <TitleCards title= {"Upcoming"}/> 
+    <TitleCards title= {"Top Pics for You"}/> 
     </div>
     </div>
   )
