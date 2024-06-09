13e254ee43ad25811e7df4e84934aefb9ea09051
Bello Israel <belloisrael1999@gmail.com>
2024-06-09 02:07:42 -0700

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,6 +30,9 @@ const Navbar = () => {
         <div className="navbar-profile">
           <img src={profile_img} alt="" className='profile' />
           <img src={caret_icon} width={10} alt="" />
+          <div className="dropdown">
+            <p>Sign out of Netflix</p>
+          </div>
         </div>
       </div>
       
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_title from '../../assets/hero_title.png'
 
 
 const Home = () => {
   return (
     <div className="home">
     <Navbar/>
+    <div className='hero'>
+      <img src='/hero_banner.jpg' alt="" className='banner-img' />
+      <div className="hero-caption">
+        <img src={hero_title} alt="" className='caption-img' />
+        <p>Discovering his ties to secret ancient irder, a young man living in modern Istanbul embarks on quest to save the city from an importal enemy.</p>
+      </div>
+    </div>
     </div>
   )
 }
