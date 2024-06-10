da125717204750a635c02fbb59ede798bb47b9ff
Bello Israel <belloisrael1999@gmail.com>
2024-06-10 12:59:29 -0700

    player adjusment added

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
@@ -7,13 +7,31 @@ import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
 
+  const [apiData, setApiData] = useState([]);
+
   const cardsRef = useRef();
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer        eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODI1Y2JiZDQwYzc4OTlkZDY3OGU2NmI3YTEyZWZiYSIsInN1YiI6IjY2NjZkYmQ4NmRkZTFjNzc3YmI5YmRlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AdWmXAEdXVZwB4x-T25ZfaDT1qfeayz6QlWH0aUbpek'
+      }
+    };
+    
+ 
+
+
 const handleWheel = (event) => {
   event.preventDefault();
   cardsRef.current.scrollLeft += event.deltaY;
 }
 
 useEffect(()=>{ 
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+  .then(response => response.json())
+  .then(response => setApiData (response.results))
+  .catch(err => console.error(err));
+
   cardsRef.current.addEventListener('wheel', handleWheel);
 
 },[])
@@ -23,10 +41,10 @@ useEffect(()=>{
     <div className="titlecards">
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) =>{
+        {apiData.map((card, index) =>{
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={ `https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
 
         })}
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+
+const Player = () => {
+  return (
+    <div className='player'>
+
+        <img src={back_arrow_icon} alt="" />
+        <iframe width='90%' height='90%' src='https://www.youtube.com/embed/1ZETzUQAzTg' title='trailer' frameBorder='0' allowFullScreen></iframe>
+     <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+     </div>
+    </div>
+  )
+}
+
+export default Player
