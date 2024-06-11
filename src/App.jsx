420bcd911c64742e18805e251184411010cdd9f2
Bello Israel <belloisrael1999@gmail.com>
2024-06-11 13:19:37 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
   useEffect(()=>{
     onAuthStateChanged(auth, async(user)=>{
-      if(user){
+      if(user){ 
         console.log("Logged In");
         navigate('/')
       }else{
