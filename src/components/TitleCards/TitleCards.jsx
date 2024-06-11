df3080ce3f385b866e98d110cb45f01fbcc89100
Bello Israel <belloisrael1999@gmail.com>
2024-06-10 20:38:18 -0700

    display added

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom'
 
 
 
@@ -42,10 +43,10 @@ useEffect(()=>{
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) =>{
-          return <div className="card" key={index}>
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
             <img src={ `https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
             <p>{card.original_title}</p>
-          </div>
+          </Link>
 
         })}
       </div>
