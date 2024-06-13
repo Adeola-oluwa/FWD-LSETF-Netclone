c8d50aa5c34d6fce89eac4482aca1c41e655e529
Bello Israel <belloisrael1999@gmail.com>
2024-06-13 13:48:59 -0700

    responsiveness

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -39,7 +39,7 @@ useEffect(()=>{
 
 
   return (
-    <div className="titlecards">
+    <div className="title-cards">
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) =>{
